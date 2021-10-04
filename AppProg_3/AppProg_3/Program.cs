b5690dfc01758be8cd6407e04dd5b9dadd1a02cd
BlizPerfect <zax_2000@mail.ru>
2021-10-04 18:51:20 +0500

    Финальная версия программы.
    Произведён финальный рефакторинг кода.

--- AppProg_3/AppProg_3/Program.cs
+++ AppProg_3/AppProg_3/Program.cs
@@ -15,51 +15,98 @@ namespace AppProg_3
         {
             InitNumber = Number;
             PersonValue = Price;
+            SortedNumber = -1;
         }
     }
 
     class Program
     {
-        static void Main(string[] args)
+        public static int InputCount()
         {
+            var error = true;
+            int result = 0;
             Console.Write("Введите количество сотрудников компании: ");
-            var count = Convert.ToInt32(Console.ReadLine());
-            List<Person> employeesList = new List<Person>();
-            List<Person> taxiDriversList = new List<Person>();
-            Random rnd = new Random();
-            for (var i = 0; i < count; i++)
+            while (error)
             {
-                employeesList.Add(new Person(i, rnd.Next(1, 1000)));
-                taxiDriversList.Add(new Person(i, rnd.Next(1, 10000)));
+                if (int.TryParse(Console.ReadLine(), out result))
+                {
+                    error = false;
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели не число, попробуйте снова.");
+                }
             }
-            Console.WriteLine("Расстояния до домов сотрудников:");
-            foreach (var employee in employeesList)
+            return result;
+        }
+
+        public static List<Person> CreatListOfPerson(int listSize, int maxListValue)
+        {
+            Random rnd = new Random();
+            List<Person> resultList = new List<Person>();
+            for (var i = 0; i < listSize; i++)
+            {
+                resultList.Add(new Person(i, rnd.Next(1, maxListValue)));
+            }
+            return resultList;
+        }
+
+        public static void ShowListValues(List<Person> list, string header)
+        {
+            Console.WriteLine(header);
+            foreach (var employee in list)
             {
                 Console.Write(employee.PersonValue + " ");
             }
-            Console.WriteLine("\nТарифы таксистов:");
-            foreach (var taxiDriver in taxiDriversList)
+        }
+
+        public static List<Person> SortList(List<Person> originalList, bool orderBy)
+        {
+            if (orderBy)
             {
-                Console.Write(taxiDriver.PersonValue + " ");
+                return originalList.OrderBy(u => u.PersonValue).ToList();
+
             }
-            var sortedListOfEmployees = employeesList.OrderBy(u => u.PersonValue).ToList();
-            for (var i = 0; i < sortedListOfEmployees.Count; i++)
+            return originalList.OrderByDescending(u => u.PersonValue).ToList();
+        }
+
+        public static void CalculateSortedIndexes(List<Person> sortdeList)
+        {
+            for (var i = 0; i < sortdeList.Count; i++)
             {
-                sortedListOfEmployees[i].SortedNumber = i;
+                sortdeList[i].SortedNumber = i;
             }
-            var sortedListOfDrivers = taxiDriversList.OrderByDescending(u => u.PersonValue).ToList();
-            for (var i = 0; i < sortedListOfDrivers.Count; i++)
+        }
+
+        public static void ShowResult(List<Person> sortedListOfDrivers, List<Person> sortedListOfEmployees, List<Person> originalEmployeesList)
+        {
+            Console.WriteLine("\n\nСамая выгодня погрузка:");
+            for (var i = 0; i < originalEmployeesList.Count; i++)
             {
-                sortedListOfDrivers[i].SortedNumber = i;
-            }
-            Console.WriteLine("\nСамая выгодня погрузка:");
-            for (var i = 0; i < employeesList.Count; i++)
-            {
-                var pos = employeesList[i].SortedNumber;
+                var pos = originalEmployeesList[i].SortedNumber;
                 int index = sortedListOfDrivers.Select((item, ind) => new { Item = item, Index = ind }).First(x => x.Item.SortedNumber == pos).Item.InitNumber;
                 Console.Write(index + " ");
             }
             Console.WriteLine("\n");
+        }
+
+        static void Main(string[] args)
+        {
+            var count = InputCount();
+
+            List<Person> employeesList = CreatListOfPerson(count, 1000);
+            List<Person> taxiDriversList = CreatListOfPerson(count, 10000);
+
+            ShowListValues(employeesList, "\nРасстояния до домов сотрудников: ");
+            ShowListValues(taxiDriversList, "\n\nТарифы таксистов");
+
+            var sortedListOfEmployees = SortList(employeesList, true);
+            CalculateSortedIndexes(sortedListOfEmployees);
+
+            var sortedListOfDrivers = SortList(taxiDriversList, false);
+            CalculateSortedIndexes(sortedListOfDrivers);
+
+            ShowResult(sortedListOfDrivers, sortedListOfEmployees, employeesList);
         }
     }
 }
