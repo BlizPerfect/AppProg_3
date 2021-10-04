d68abb5fe6ff251f8c43055b70184da331687639
BlizPerfect <zax_2000@mail.ru>
2021-10-04 17:20:14 +0500

    Проведён начальный рефакторинг кода

--- AppProg_3/AppProg_3/Program.cs
+++ AppProg_3/AppProg_3/Program.cs
@@ -32,48 +32,34 @@ namespace AppProg_3
                 employeesList.Add(new Person(i, rnd.Next(1, 1000)));
                 taxiDriversList.Add(new Person(i, rnd.Next(1, 10000)));
             }
-            //Выводится N чисел.Первое число — номер такси, в которое должен сесть первый сотрудник, второе число — номер такси, в которое должен сесть второй и т.д.,
-            Console.WriteLine("Начальные данные:");
             Console.WriteLine("Расстояния до домов сотрудников:");
             foreach (var employee in employeesList)
             {
-                Console.WriteLine(employee.PersonValue + " " + employee.InitNumber);
+                Console.Write(employee.PersonValue + " ");
             }
             Console.WriteLine("\nТарифы таксистов:");
             foreach (var taxiDriver in taxiDriversList)
             {
-                Console.WriteLine(taxiDriver.PersonValue + " " + taxiDriver.InitNumber);
+                Console.Write(taxiDriver.PersonValue + " ");
             }
-            Console.WriteLine("\nРастояния, отсортированные по убыванию:");
             var sortedListOfEmployees = employeesList.OrderBy(u => u.PersonValue).ToList();
             for (var i = 0; i < sortedListOfEmployees.Count; i++)
             {
                 sortedListOfEmployees[i].SortedNumber = i;
             }
-            foreach (var i in sortedListOfEmployees)
-            {
-                Console.WriteLine("Value: " + i.PersonValue + " InitNumber:" + i.InitNumber + " SortedNumber:" + i.SortedNumber);
-            }
-
-            Console.WriteLine("Тарифы, отсортированные по убыванию:");
             var sortedListOfDrivers = taxiDriversList.OrderByDescending(u => u.PersonValue).ToList();
             for (var i = 0; i < sortedListOfDrivers.Count; i++)
             {
                 sortedListOfDrivers[i].SortedNumber = i;
             }
-            foreach (var i in sortedListOfDrivers)
-            {
-                Console.WriteLine("Value: " + i.PersonValue + " InitNumber:" + i.InitNumber + " SortdeNumber:" + i.SortedNumber);
-            }
-
-            Console.WriteLine("");
+            Console.WriteLine("\nСамая выгодня погрузка:");
             for (var i = 0; i < employeesList.Count; i++)
             {
                 var pos = employeesList[i].SortedNumber;
                 int index = sortedListOfDrivers.Select((item, ind) => new { Item = item, Index = ind }).First(x => x.Item.SortedNumber == pos).Item.InitNumber;
-                var result = sortedListOfDrivers[index].InitNumber;
                 Console.Write(index + " ");
             }
+            Console.WriteLine("\n");
         }
     }
 }
